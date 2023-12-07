4198db93eac2baf6d80e141257d3bb277a4b2e24
knightalli <knightalli@mail.ru>
2023-12-07 23:41:30 +0500

    react2

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,17 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "../components/post/Post";
+import Modal from "../components/Modal/Modal";
+import axios from "axios";
 
 const App = () => {
-  function increment(value) {
-    setCounter(value);
-    console.log(value);
+  function addPost(post) {
+    const newPost = {
+      ...post,
+      id: new Date().getMilliseconds(),
+      userId: 1,
+    };
+    setPosts([...posts, newPost]);
   }
-  const [counter, setCounter] = useState(0);
+
+  const [input, setInput] = useState("");
+
+  const [open, setOpen] = useState(false);
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.data)
+      .then((data) => setPosts(data))
+      .catch((err) => setPosts([]));
+  }, []);
 
   return (
-    <div>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      {counter}
-      <button onClick={() => increment(counter+1)}>+</button>
+    <div className="container">
+      <input
+        className="post-input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Создать"
+      />
+      {posts.length ? (
+        <div className="post-list">
+          {posts.map((post) => (
+            <Post key={post.id} data={post} />
+          ))}
+        </div>
+      ) : (
+        <h2>Постов нет</h2>
+      )}
+
+      <button className="post-add" onClick={() => setOpen(true)}>
+        Добавить
+      </button>
+      <Modal open={open} addPost={addPost} close={() => setOpen(false)} />
     </div>
   );
 };
